441d9b83ec9919255baf6f2043845afdd3fc617f
George Nunes <georgebnunes@gmail.com>
2026-05-21 21:36:24 -0300

    fix: return a pointer

--- internal/repository/crud_repository.go
+++ internal/repository/crud_repository.go
@@ -3,9 +3,9 @@ package repository
 import "context"
 
 type CrudRepository[T any] interface {
-	Create(ctx context.Context, item T) (T, error)
-	Get(ctx context.Context, id string) (T, error)
+	Create(ctx context.Context, item T) (*T, error)
+	Get(ctx context.Context, id string) (*T, error)
 	GetAll(ctx context.Context) ([]T, error)
-	Update(ctx context.Context, item T) (T, error)
+	Update(ctx context.Context, item T) (*T, error)
 	Delete(ctx context.Context, id string) error
 }
