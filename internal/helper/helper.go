316323674331d3ef49a5d0ae881e32260f28d728
George Nunes <georgebnunes@gmail.com>
2026-05-23 15:33:57 -0300

    feat: extract json from requests

--- internal/helper/helper.go
+++ internal/helper/helper.go
@@ -2,6 +2,7 @@ package helper
 
 import (
 	"encoding/json"
+	"log/slog"
 	"net/http"
 )
 
@@ -9,4 +10,14 @@ func WriteJSON(w http.ResponseWriter, status int, data any) error {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(status)
 	return json.NewEncoder(w).Encode(data)
+}
+
+func ReadJSON(r *http.Request, dst any) error {
+
+	if err := json.NewDecoder(r.Body).Decode(&dst); err != nil {
+		slog.Error("Error when parsing the JSON Body")
+		return err
+	}
+
+	return nil
 }
