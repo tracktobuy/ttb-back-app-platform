ddd34c35837d0bcba88a907bcd5dfa2512df5685
George Nunes <georgebnunes@gmail.com>
2026-05-23 22:20:36 -0300

    feat: helper to print internal server error

--- internal/helper/helper.go
+++ internal/helper/helper.go
@@ -12,12 +12,28 @@ func WriteJSON(w http.ResponseWriter, status int, data any) error {
 	return json.NewEncoder(w).Encode(data)
 }
 
-func ReadJSON(r *http.Request, dst any) error {
+func ReadJSON(w http.ResponseWriter, r *http.Request, dst any) error {
 
-	if err := json.NewDecoder(r.Body).Decode(&dst); err != nil {
+	r.Body = http.MaxBytesReader(w, r.Body, 1048576)
+
+	dec := json.NewDecoder(r.Body)
+	dec.DisallowUnknownFields()
+
+	err := dec.Decode(&dst)
+	if err != nil {
 		slog.Error("Error when parsing the JSON Body")
 		return err
 	}
 
 	return nil
+}
+
+func BadRequest(w http.ResponseWriter, err error) {
+	slog.Error("Bad request: %+v", err)
+	WriteJSON(w, http.StatusBadRequest, map[string]any{"error": err.Error()})
+}
+
+func InternalServerError(w http.ResponseWriter, err error) {
+	slog.Error("Internal server error: %+v", err)
+	WriteJSON(w, http.StatusInternalServerError, map[string]any{"error": "Internal Server Error"})
 }
