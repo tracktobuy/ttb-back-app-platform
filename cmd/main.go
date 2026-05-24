0e65f291b8adfa96f6619d50e4c6e7d233b0d209
George Nunes <georgebnunes@gmail.com>
2026-05-23 22:21:05 -0300

    feat: binding user service creation

--- cmd/main.go
+++ cmd/main.go
@@ -5,12 +5,16 @@ import (
 	"net/http"
 
 	"github.com/tracktobuy/ttb-back-app-platform/internal/handler"
+	"github.com/tracktobuy/ttb-back-app-platform/internal/repository"
+	"github.com/tracktobuy/ttb-back-app-platform/internal/service"
 )
 
 func main() {
 	mux := http.NewServeMux()
 
-	userHandler := handler.NewUserHandler()
+	repository := repository.NewUserRepo(nil)
+	service := service.NewUserService(repository)
+	userHandler := handler.NewUserHandler(service)
 	userHandler.Routes(mux)
 
 	log.Printf("Server is running on port %s", ":8080")
