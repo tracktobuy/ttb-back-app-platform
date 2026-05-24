e58bdff7e735b41219ae6386c928912e93bdc416
George Nunes <georgebnunes@gmail.com>
2026-05-23 22:20:50 -0300

    feat: full implementation for user creation

--- internal/handler/user_handler.go
+++ internal/handler/user_handler.go
@@ -1,8 +1,8 @@
 package handler
 
 import (
+	"context"
 	"log"
-	"log/slog"
 	"net/http"
 
 	"github.com/tracktobuy/ttb-back-app-platform/internal/domain"
@@ -15,11 +15,13 @@ type UserHandler struct {
 }
 
 type userRequest struct {
-	Hey string `json:"hey"`
+	UUID     string `json:"uuid"`
+	Name     string `json:"name"`
+	Username string `json:"username"`
 }
 
-func NewUserHandler() *UserHandler {
-	return &UserHandler{service: nil}
+func NewUserHandler(service service.CrudService[domain.User]) *UserHandler {
+	return &UserHandler{service: service}
 }
 
 func (h *UserHandler) Routes(mux *http.ServeMux) {
@@ -27,12 +29,31 @@ func (h *UserHandler) Routes(mux *http.ServeMux) {
 }
 
 func (h *UserHandler) Create(w http.ResponseWriter, r *http.Request) {
-	slog.Info("Received request: %s %s", r.Method, r.URL.Path)
+	log.Printf("Received request: %s %s", r.Method, r.URL.Path)
 
 	var userRequest userRequest
-	helper.ReadJSON(r, &userRequest)
+	err := helper.ReadJSON(w, r, &userRequest)
+
+	if err != nil {
+		helper.BadRequest(w, err)
+		return
+	}
+
+	item := domain.User{
+		UUID:     userRequest.UUID,
+		Name:     userRequest.Name,
+		Username: userRequest.Username,
+	}
+
+	user, err := h.service.Create(context.Background(), item)
+
+	if err != nil {
+		helper.InternalServerError(w, err)
+		return
+	}
 
 	log.Printf("Parsed request body: %+v", userRequest)
+	log.Printf("Created user: %+v", user)
 
-	helper.WriteJSON(w, http.StatusCreated, map[string]any{"data": "somethings"})
+	helper.WriteJSON(w, http.StatusCreated, map[string]any{"data": user})
 }
