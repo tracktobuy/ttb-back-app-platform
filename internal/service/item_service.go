1dfab03be0c80f17034d2072b5a98915aeae84fc
George Nunes <georgebnunes@gmail.com>
2026-06-01 17:46:08 -0300

    feat: item service with create function

---
+++ internal/service/item_service.go
@@ -0,0 +1,60 @@
+package service
+
+import (
+	"context"
+	"time"
+
+	"github.com/tracktobuy/ttb-back-app-platform/internal/domain"
+	"github.com/tracktobuy/ttb-back-app-platform/internal/helper"
+	"github.com/tracktobuy/ttb-back-app-platform/internal/repository"
+)
+
+type ItemServiceInterface interface {
+	CrudService[domain.Item]
+	GetAllByGroupID(ctx context.Context, groupID string) ([]domain.Item, error)
+}
+
+type itemService struct {
+	repo repository.CrudRepository[domain.Item]
+}
+
+func NewItemService(repo repository.CrudRepository[domain.Item]) ItemServiceInterface {
+	return &itemService{
+		repo: repo,
+	}
+}
+
+func (s *itemService) Create(ctx context.Context, item domain.Item) (*domain.Item, error) {
+
+	item.UUID = helper.GenerateUUIDV7()
+	item.Version = 1
+	item.CreatedAt = time.Now().UTC()
+
+	newItem, err := s.repo.Create(ctx, item)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return newItem, nil
+}
+
+func (s *itemService) Get(ctx context.Context, id string) (*domain.Item, error) {
+	return nil, nil
+}
+
+func (s *itemService) GetAll(ctx context.Context) ([]domain.Item, error) {
+	return []domain.Item{}, nil
+}
+
+func (s *itemService) Update(ctx context.Context, id string, item domain.Item) (*domain.Item, error) {
+	return nil, nil
+}
+
+func (s *itemService) Delete(ctx context.Context, id string) error {
+	return nil
+}
+
+func (s *itemService) GetAllByGroupID(ctx context.Context, groupID string) ([]domain.Item, error) {
+	return []domain.Item{}, nil
+}
